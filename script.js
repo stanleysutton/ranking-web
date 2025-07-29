7be8191fd91206fac229c6cf0433826ce2089cdd
stan <stanleysutton2510@gmail.com>
2025-07-29 17:20:54 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+function inviteBot() {
+  window.open('https://discord.com/oauth2/authorize?client_id=1394928756963020912&permissions=1085486394576&integration_type=0&scope=bot+applications.commands', '_blank');
+}
